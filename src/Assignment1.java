4dfa7cdaee64243c877ecc4393f4663498a3500f
swayampandey <swayam265@gmail.com>
2022-06-21 18:57:57 +0530

    added: addition and subtraction

---
+++ src/Assignment1.java
@@ -0,0 +1,12 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		int a=40;
+		int b=50;
+		System.out.println("a+b= " +(a+b) );
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Subtraction.java
@@ -0,0 +1,12 @@
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		int a=40;
+		int b=20;
+		System.out.println("a-b="+(a-b));
+		// TODO Auto-generated method stub
+
+	}
+
+}
